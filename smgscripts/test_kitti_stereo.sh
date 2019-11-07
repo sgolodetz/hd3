b4b5750a34353d36144b96f31a0a4bdada854636
Stuart Golodetz <sgolodetz@gxstudios.net>
2019-11-07 15:14:08 +0000

    Rerun the network to output the confidence maps for the various sequences.

--- smgscripts/test_kitti_stereo.sh
+++ smgscripts/test_kitti_stereo.sh
@@ -1,7 +1,7 @@
 python -u inference.py \
   --task=stereo \
   --data_root=/media/data/datasets \
-  --data_list=smglists/KITTI_raw_stereo_2011_09_26_drive_0005_sync.txt \
+  --data_list=smglists/KITTI_raw_stereo_2011_09_26_drive_0011_sync.txt \
   --context \
   --encoder=dlaup \
   --decoder=hda \
@@ -9,4 +9,4 @@ python -u inference.py \
   --workers=16 \
   --flow_format=png \
   --model_path=model_zoo/hd3sc_things_kitti-368975c0.pth \
-  --save_folder=/media/data/datasets/hd3_prob/kitti_raw_stereo
+  --save_folder=/media/data/datasets/hd3/kitti_raw_stereo
