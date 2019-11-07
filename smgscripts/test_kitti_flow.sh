8d8e51af6603a38ca8d1ddb1416c5820744dfc80
Stuart Golodetz <sgolodetz@gxstudios.net>
2019-11-07 12:14:24 +0000

    Try yet another sequence.

--- smgscripts/test_kitti_flow.sh
+++ smgscripts/test_kitti_flow.sh
@@ -1,7 +1,7 @@
 python -u inference.py \
   --task=flow \
   --data_root=/media/data/datasets \
-  --data_list=smglists/KITTI_raw_flow_2011_09_26_drive_0009_sync.txt \
+  --data_list=smglists/KITTI_raw_flow_2011_09_26_drive_0011_sync.txt \
   --context \
   --encoder=dlaup \
   --decoder=hda \
--- smgscripts/test_kitti_stereo.sh
+++ smgscripts/test_kitti_stereo.sh
@@ -1,7 +1,7 @@
 python -u inference.py \
   --task=stereo \
   --data_root=/media/data/datasets \
-  --data_list=smglists/KITTI_raw_stereo_2011_09_26_drive_0009_sync.txt \
+  --data_list=smglists/KITTI_raw_stereo_2011_09_26_drive_0011_sync.txt \
   --context \
   --encoder=dlaup \
   --decoder=hda \
