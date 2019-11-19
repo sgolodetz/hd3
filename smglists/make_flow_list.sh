5f2e8ec0576d49c94362b522bb6d5054301e5622
Stuart Golodetz <sgolodetz@gxstudios.net>
2019-11-19 15:19:50 +0000

    Add a script to make the flow lists.

---
+++ smglists/make_flow_list.sh
@@ -0,0 +1,15 @@
+#! /bin/bash -e
+
+rm -f flow*.txt
+
+ls /media/data/datasets/kitti_raw/$1/$2/image_02/data | while read f
+do
+	echo "kitti_raw/$1/$2/image_02/data/$f" >> flow1.txt
+	cp flow1.txt flow2.txt
+	head -n -1 flow1.txt > flow3.txt
+	tail -n +2 flow2.txt > flow4.txt
+	paste -d ' ' flow3.txt flow4.txt > "KITTI_raw_flow_$2.txt"
+done
+
+rm -f flow*.txt
+
