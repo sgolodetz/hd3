306fa7d7f1c404b39260267a635eaccc23f0d3b1
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-03-19 14:22:02 +0000

    Add make_kitti_flow.bat.

---
+++ smgscripts/make_kitti_flow.bat
@@ -0,0 +1,1 @@
+python -u inference.py --task=flow --data_root="D:/KittiDataset" --data_list="smglists/KITTI_raw_flow_%1.txt" --context --encoder=dlaup --decoder=hda --batch_size=1 --workers=16 --flow_format=flo --model_path="model_zoo/hd3fc_chairs_things_kitti-bfa97911.pth" --save_folder="D:/HD3Data/kitti_raw_flow2d"
