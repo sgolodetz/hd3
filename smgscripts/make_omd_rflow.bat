86afde83856b779dce9f1d8c7aad59b6f58b6987
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-03-20 11:14:04 +0000

    Add make_omd_flow.bat and make_omd_rflow.bat.

---
+++ smgscripts/make_omd_rflow.bat
@@ -0,0 +1,1 @@
+python -u inference.py --task=flow --data_root="D:/datasets" --data_list="smglists/omd_rflow_%1_%2.txt" --context --encoder=dlaup --decoder=hda --batch_size=1 --workers=16 --flow_format=flo --model_path="model_zoo/hd3fc_chairs_things_kitti-bfa97911.pth" --save_folder="D:/datasets/hd3/omd_rflow2d"
